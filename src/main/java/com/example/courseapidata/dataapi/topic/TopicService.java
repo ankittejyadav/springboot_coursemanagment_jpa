279e916801be19035563fd50714b731457439285
Ankit Yadav <ankittejyadav@gmail.com>
2023-07-15 11:37:47 -0400

    first commit

---
+++ src/main/java/com/example/courseapidata/dataapi/topic/TopicService.java
@@ -0,0 +1,46 @@
+package com.example.courseapidata.dataapi.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("CS101", "Intro", "Beginner"),
+            new Topic("CS102", "Algos", "Med"),
+            new Topic("CS201", "Adv Prog", "Expert")
+        ));
+
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+
+    public Topic getTopic(String id){
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public Topic getTopicName(String name){
+        return topics.stream().filter(t -> t.getName().equals(name)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic){
+        for(int i=0;i<topics.size();i++){
+            Topic t = topics.get(i);
+            if (t.getId().equals(id)){
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id){
+        topics.removeIf(t -> t.getId().equals(id));
+    }
+}
---
+++ src/test/java/com/example/courseapidata/dataapi/DataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseapidata.dataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
